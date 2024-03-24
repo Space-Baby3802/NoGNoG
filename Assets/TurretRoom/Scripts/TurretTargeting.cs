36826142da2721932c4d9140880b2215407c436f
Justin Derot <justinderot@hotmail.ca>
2024-03-23 21:37:35 -0700

    TurretProgress

--- Assets/TurretRoom/Scripts/TurretTargeting.cs
+++ Assets/TurretRoom/Scripts/TurretTargeting.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TurretTargeting : MonoBehaviour
 {
     [SerializeField] private Transform rayShooter;
     [SerializeField] private LayerMask player;
+
+    [SerializeField] private AudioSource turretActive;
+    [SerializeField] private GameObject muzzleCanvas;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +19,35 @@ public class TurretTargeting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Physics.Raycast(rayShooter.position, Vector3.left, 1000f, player))
+
+        if (Physics.Raycast(rayShooter.position, Vector3.left, out RaycastHit hit, 1000f))
         {
-            Debug.DrawRay(rayShooter.position, Vector3.left, Color.cyan);
-            Debug.Log("I can see you!");
+            if (hit.collider.gameObject.layer == 8)
+            {
+                Debug.DrawRay(rayShooter.position, Vector3.left, Color.cyan);
+                Debug.Log("I can see you!");
+                turretActive.Play();
+                if (turretActive.isPlaying)
+                {
+                    MuzzleFlash();
+                }
+            }
+            else
+            {
+              Debug.Log(hit.collider.gameObject.layer);
+            }
         }
-        
+    }
+
+    public void MuzzleFlash()
+    {
+        muzzleCanvas.SetActive(true);
+        MuzzleFlashOff();
+    }
+    public void MuzzleFlashOff()
+    {
+        muzzleCanvas?.SetActive(false);
+        MuzzleFlash();
     }
 
 
